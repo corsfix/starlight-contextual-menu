42751fe0385c6b3303d1cc612cf9140ed304acc2
Reynaldi Chernando <reynaldichernando@gmail.com>
2025-08-05 21:38:20 +0700

    Fix cloudflare license

--- packages/starlight-contextual-menu/contextual-menu.js
+++ packages/starlight-contextual-menu/contextual-menu.js
@@ -5,13 +5,9 @@ const DEFAULT_ACTIONS = {
     className: "copy-action",
     action: async () => {
       /**
-       * The MIT License (MIT)
-       *
-       * Copyright (c) 2021 Cloudflare, Inc.
-       *
-       * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction,
-       * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-       * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+       * The MIT License (MIT) Copyright (c) 2021 Cloudflare, Inc.
+       * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+       * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
        */
       const markdownUrl = new URL("index.md", window.location.href).toString();
       try {
