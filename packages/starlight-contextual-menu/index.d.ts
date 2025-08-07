7986c2f36934d848bf95790771d62a95fa2ba387
Reynaldi Chernando <reynaldichernando@gmail.com>
2025-08-07 15:02:31 +0700

    Init custom action

--- packages/starlight-contextual-menu/index.d.ts
+++ packages/starlight-contextual-menu/index.d.ts
@@ -2,8 +2,14 @@ import type { StarlightPlugin } from "@astrojs/starlight/types";
 
 type ContextualMenuActionType = "copy" | "view" | "claude" | "chatgpt";
 
+interface ContextualMenuActionObject {
+  label: string;
+  icon?: string;
+  action: () => void;
+}
+
 export interface StarlightContextualMenuUserConfig {
-  actions: ContextualMenuActionType[];
+  actions: (ContextualMenuActionType | ContextualMenuActionObject)[];
 }
 
 export default function starlightContextualMenu(
